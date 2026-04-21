6296bdeb51ea716a2129723afe1081cbebf3bbae
alex2sc <scolnicalexandru@gmail.com>
2026-04-21 14:22:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,41 @@ window.addEventListener('DOMContentLoaded', () => {
         document.getElementById('dark-toggle').textContent = '☀️';
     }
 });
+
+let images = [
+        "../images/slide1.jpg", 
+        "../images/slide2.jpg",
+        "../images/slide3.jpg",
+    ];
+let index = 0;
+
+function showSlide() {
+    document.getElementById("slide").src = images[index];
+}
+
+function nextSlide() {
+    index = (index + 1) % images.length;
+    showSlide();
+}
+
+function prevSlide() {
+    index = (index - 1 + images.length) % images.length;
+    showSlide();
+}
+
+// SWIPE (telefon)
+let startX = 0;
+const slideImg = document.getElementById("slide");
+
+slideImg.addEventListener("touchstart", function(e) {
+    startX = e.touches[0].clientX;
+});
+
+slideImg.addEventListener("touchend", function(e) {
+    let endX = e.changedTouches[0].clientX;
+
+    if (startX > endX + 50) nextSlide(); // swipe stânga
+    if (startX < endX - 50) prevSlide(); // swipe dreapta
+});
+
+let autoSlide = setInterval(nextSlide, 3000);
